efd423876f7b2e95e8e1df90f07f40e028ebc6fd
Krishnesh Meratia <63016242+kronecker08@users.noreply.github.com>
2020-11-21 22:03:12 -0500

    Update ORBextractor.cc

--- src/ORBextractor.cc
+++ src/ORBextractor.cc
@@ -57,7 +57,7 @@
 
 //#include <opencv2/core/core.hpp>
 //#include <opencv2/highgui/highgui.hpp>
-#include <opencv/opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <vector>
 
 #include "ORBextractor.h"
